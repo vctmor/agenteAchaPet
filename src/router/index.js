07a63158372be810f51bdf8df0a71d849a6251a2
Victor Moreira <you@example.com>
2025-08-04 11:30:04 -0300

    thumbnail

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import Testes from '@/views/Testes.vue'
 import FormSearch from '@/views/FormSearch.vue'
 import CartazView from '@/views/CartazView.vue'
 import ListView from '@/views/ListView.vue'
-import HomePage from '@/views/Sobre.vue'
+import Sobre from '@/views/Sobre.vue'
 import Contato from '@/views/Contato.vue'
 
 const routes = [
