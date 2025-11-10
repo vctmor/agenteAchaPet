f25aaa7020e90a0f8023c12851c9ddc23a9e9b86
Victor Moreira <you@example.com>
2025-11-10 11:37:08 -0300

    alterado api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 // util/api.js
 export function apiUrl(path = '') {
-  const base = (import.meta.env.VITE_API_URL || 'http://agenteachapet.onrender.com/api/v1')
+  const base = ('http://agenteachapet.onrender.com/api/v1')
     .replace(/\/+$/, '');
   return `${base}/${String(path).replace(/^\/+/, '')}`;
 }
